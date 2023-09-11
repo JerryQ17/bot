f740a43781b9866274ccc330bce63cd22491aee6
JerryQ <1397200108@qq.com>
2023-09-12 00:13:32 +0800

    略改进gocqhttp的API请求方式

--- src/gocqhttp/api/account.rs
+++ src/gocqhttp/api/account.rs
@@ -1,11 +1,42 @@
+use serde::Deserialize;
+
 use crate::Result;
 use crate::gocqhttp::GoCqhttp;
 use crate::gocqhttp::api::APIResponse;
 
+#[derive(Deserialize)]
+pub struct LoginInfo {
+    pub user_id: i64,
+    pub nickname: String,
+}
+
 
 impl GoCqhttp {
-    async fn get_login_info(&self) -> Result<()> {
+    /// [获取登录号信息](https://docs.go-cqhttp.org/api/#%E8%8E%B7%E5%8F%96%E7%99%BB%E5%BD%95%E5%8F%B7%E4%BF%A1%E6%81%AF)
+    pub async fn get_login_info(&self) -> Result<LoginInfo> {
         let resp = self.get("/get_login_info").send().await?;
+        Ok(APIResponse::<LoginInfo>::from_response(resp)
+            .await?
+            .unwrap_data()
+            .unwrap())
+    }
+
+    /// [设置登录号资料](https://docs.go-cqhttp.org/api/#%E8%AE%BE%E7%BD%AE%E7%99%BB%E5%BD%95%E5%8F%B7%E8%B5%84%E6%96%99)
+    pub async fn set_qq_profile(
+        &self,
+        nickname: String, company: String, email: String, college: String, personal_note: String,
+    ) -> Result<()> {
+        let params = [
+            ("nickname", nickname),
+            ("company", company),
+            ("email", email),
+            ("college", college),
+            ("personal_note", personal_note),
+        ];
+        let resp = self.post("/set_qq_profile")
+            .query(&params)
+            .send()
+            .await?;
         APIResponse::<()>::from_response(resp).await?;
         Ok(())
     }
--- src/gocqhttp/api/mod.rs
+++ src/gocqhttp/api/mod.rs
@@ -1,14 +1,16 @@
 mod account;
 
+use std::io::Error;
+use std::io::ErrorKind;
 use serde::Deserialize;
 use serde::de::DeserializeOwned;
 use reqwest::{RequestBuilder, Response};
 
-
 use crate::Result;
-use super::GoCqhttp;
+use crate::gocqhttp::GoCqhttp;
 
-#[derive(Deserialize)]
+
+#[derive(Deserialize, Debug)]
 struct APIResponse<T> {
     status: String,
     retcode: i32,
@@ -19,22 +21,64 @@ struct APIResponse<T> {
 }
 
 
+// 从API响应中提取数据
 impl<T: DeserializeOwned> APIResponse<T> {
-    pub async fn from_response(response: Response) -> Result<Self> {
-        Ok(response
-            .json::<APIResponse<T>>()
-            .await?)
+    async fn from_response(response: Response) -> Result<Self> {
+        let status = response.status().as_u16();
+        match status {
+            200 | 401 | 403 | 404 | 406 => Ok(response.json::<APIResponse<T>>().await?),
+            _ => {
+                let msg = format!("执行API请求失败，代码{}: {}", status, response.text().await?);
+                Err(Error::new(ErrorKind::Other, msg).into())
+            }
+        }
     }
 
-    fn unwrap(self) -> Result<Option<T>> {
-        if self.status == "ok" {
+    fn data(self) -> Result<Option<T>> {
+        if self.is_ok() {
             Ok(self.data)
+        } else if self.is_async() {
+            Err(Error::new(
+                ErrorKind::Other,
+                "已提交异步处理，请等待处理完成",
+            ).into())
         } else {
-            Err(self.message.unwrap().into())
+            let msg = self.message.unwrap_or("API Request Failed".to_string());
+            let detail = self.wording.unwrap_or("Unknown Reason".to_string());
+            Err(Error::new(
+                ErrorKind::Other,
+                format!("{}: {}", msg, detail),
+            ).into())
+        }
+    }
+
+    fn unwrap_data(self) -> Result<T> {
+        let data = self.data()?;
+        match data {
+            Some(data) => Ok(data),
+            None => Err(Error::new(
+                ErrorKind::Other,
+                "API Request Failed: No Data",
+            ).into())
         }
     }
 }
 
+
+// 检查API请求状态
+impl<T> APIResponse<T> {
+    fn is_ok(&self) -> bool {
+        self.status == "ok" || self.retcode == 0
+    }
+
+    fn is_async(&self) -> bool {
+        self.status == "async" || self.retcode == 1
+    }
+
+    fn is_failed(&self) -> bool {
+        self.status == "failed" || (self.retcode != 0 && self.retcode != 1)
+    }
+}
 
 impl GoCqhttp {
     pub fn get(&self, endpoint: &str) -> RequestBuilder {
@@ -43,5 +87,95 @@ impl GoCqhttp {
 
     pub fn post(&self, endpoint: &str) -> RequestBuilder {
         self.client.post(self.server.to_string() + endpoint)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Deserialize, Debug, PartialEq)]
+    struct TestStruct {
+        a: i32,
+        b: String,
+    }
+
+    #[derive(Deserialize, Debug, PartialEq)]
+    struct TestEmptyStruct {}
+
+    #[test]
+    fn test_api_response() {
+        let json = r#"{
+            "status": "ok",
+            "retcode": 0,
+            "message": "success",
+            "data": {
+                "a": 123456,
+                "b": "nickname"
+            }
+        }"#;
+        let resp = serde_json::from_str::<APIResponse<TestStruct>>(json).unwrap();
+        assert!(resp.is_ok());
+        let data = resp.unwrap_data().unwrap();
+        assert_eq!(data, TestStruct { a: 123456, b: "nickname".to_string() });
+        assert_eq!(data.a, 123456);
+        assert_eq!(data.b, "nickname");
+    }
+
+
+    #[test]
+    fn test_api_response_async() {
+        let json = r#"{
+            "status": "async",
+            "retcode": 1,
+            "message": "async",
+            "wording": "async"
+        }"#;
+        let resp = serde_json::from_str::<APIResponse<TestStruct>>(json).unwrap();
+        assert!(resp.is_async());
+        assert_eq!(resp.message.unwrap(), "async");
+        assert_eq!(resp.wording.unwrap(), "async");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_api_response_failed() {
+        let json = r#"{
+            "status": "failed",
+            "retcode": 100,
+            "message": "Request Failed",
+            "wording": "there is no data"
+        }"#;
+        let resp = serde_json::from_str::<APIResponse<TestStruct>>(json).unwrap();
+        assert!(resp.is_failed());
+        resp.data().unwrap();
+    }
+
+    #[test]
+    fn test_api_response_empty() {
+        let json = r#"{
+            "status": "ok",
+            "retcode": 0,
+            "message": "success",
+            "data": {}
+        }"#;
+        let resp = serde_json::from_str::<APIResponse<TestEmptyStruct>>(json).unwrap();
+        assert!(resp.is_ok());
+        let data = resp.data().unwrap();
+        assert_eq!(data.unwrap(), TestEmptyStruct {});
+    }
+
+    #[test]
+    fn test_api_response_null() {
+        let json = r#"{
+            "status": "ok",
+            "retcode": 0,
+            "message": "success",
+            "data": null
+        }"#;
+        let resp = serde_json::from_str::<APIResponse<()>>(json).unwrap();
+        assert!(resp.is_ok());
+        dbg!(&resp);
+        assert!(resp.data().is_ok());
     }
 }
