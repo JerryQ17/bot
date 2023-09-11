878df632c1eb4801c4d518bf199cc5ce53d11d2a
JerryQ <1397200108@qq.com>
2023-09-11 22:24:17 +0800

    为Error实现了From<reqwest::Error>特征

--- src/error.rs
+++ src/error.rs
@@ -26,10 +26,8 @@ impl From<std::io::Error> for Error {
 }
 
 
-impl TryInto<std::io::Error> for Error {
-    type Error = std::io::Error;
-
-    fn try_into(self) -> Result<std::io::Error, Self::Error> {
-        Err(std::io::Error::new(std::io::ErrorKind::Other, self.msg))
+impl From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Self {
+        Error::new(&err.to_string())
     }
 }
