4f25199cf5285078c15dcf1f7557e4bc672ee4cd
JerryQ <1397200108@qq.com>
2023-09-13 15:04:10 +0800

    封装了好友信息的API

---
+++ src/gocqhttp/api/friend_info.rs
@@ -0,0 +1,110 @@
+use serde::Deserialize;
+
+use crate::Result;
+use crate::gocqhttp::GoCqhttp;
+use crate::gocqhttp::api::APIResponse;
+
+/// `get_stranger_info`API的响应数据结构
+#[derive(Deserialize)]
+pub struct StrangeInfo {
+    /// QQ号
+    user_id: i64,
+    /// 昵称
+    nickname: String,
+    /// 性别, male 或 female 或 unknown
+    sex: String,
+    /// 年龄
+    age: i32,
+    /// qid ID身份卡
+    qid: String,
+    /// 等级
+    level: i32,
+    /// 等级
+    login_days: i32,
+}
+
+/// `get_friend_list`API的响应数据结构
+#[derive(Deserialize)]
+pub struct Friend {
+    /// QQ号
+    user_id: i64,
+    /// 昵称
+    nickname: String,
+    /// 备注名
+    remark: String,
+}
+
+
+/// `get_unidirectional_friend_list`API的响应数据结构
+#[derive(Deserialize)]
+pub struct UnidirectionalFriend {
+    /// QQ号
+    user_id: i64,
+    /// 昵称
+    nickname: String,
+    /// 来源
+    source: String,
+}
+
+
+impl GoCqhttp {
+    /// [获取陌生人信息](https://docs.go-cqhttp.org/api/#%E8%8E%B7%E5%8F%96%E9%99%8C%E7%94%9F%E4%BA%BA%E4%BF%A1%E6%81%AF)
+    pub async fn get_stranger_info(&self, user_id: i64, no_cache: bool) -> Result<StrangeInfo> {
+        let resp = self
+            .get("/get_stranger_info")
+            .query(&[("user_id", user_id)])
+            .query(&[("no_cache", no_cache)])
+            .send()
+            .await?;
+        APIResponse::<StrangeInfo>::from_response(resp)
+            .await?
+            .data()
+    }
+
+    /// [获取好友列表](https://docs.go-cqhttp.org/api/#%E8%8E%B7%E5%8F%96%E5%A5%BD%E5%8F%8B%E5%88%97%E8%A1%A8)
+    pub async fn get_friend_list(&self) -> Result<Vec<Friend>> {
+        let resp = self
+            .get("/get_friend_list")
+            .send()
+            .await?;
+        APIResponse::<Vec<Friend>>::from_response(resp)
+            .await?
+            .data()
+    }
+
+    /// [获取单向好友列表](https://docs.go-cqhttp.org/api/#%E8%8E%B7%E5%8F%96%E5%8D%95%E5%90%91%E5%A5%BD%E5%8F%8B%E5%88%97%E8%A1%A8)
+    pub async fn get_unidirectional_friend_list(&self) -> Result<Vec<UnidirectionalFriend>> {
+        let resp = self
+            .get("/get_unidirectional_friend_list")
+            .send()
+            .await?;
+        APIResponse::<Vec<UnidirectionalFriend>>::from_response(resp)
+            .await?
+            .data()
+    }
+}
+
+
+// 仅测试不会改变QQ的状态的API
+#[cfg(test)]
+mod tests {
+    use super::super::setup_gocqhttp_for_api_test::setup;
+
+    #[tokio::test]
+    async fn test_get_strange_info() {
+        let gch = setup();
+        assert!(gch.get_stranger_info(3364697503, false).await.is_ok())
+    }
+
+    #[tokio::test]
+    async fn test_get_friend_list() {
+        let gch = setup();
+        assert!(gch.get_friend_list().await.is_ok())
+    }
+
+    #[tokio::test]
+    async fn test_get_unidirectional_friend_list() {
+        let gch = setup();
+        assert!(gch.get_unidirectional_friend_list().await.is_ok())
+    }
+}
